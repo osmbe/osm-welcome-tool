d1f17a0a6b0d2642c6575f7e890216c28f713a7f
M1dgard <M1dgard@users.noreply.github.com>
2018-07-31 16:12:59 +0200

    Add license: AGPLv3

--- includes/files/print_list_of_contributors.php
+++ includes/files/print_list_of_contributors.php
@@ -1,4 +1,18 @@
 <?php
+/* This file is part of osm-welcome: a platform to coordinate welcoming of OpenStreetMap mappers
+ * Copyright © 2018  Midgard and osm-welcome contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 include_once(INCLUDES_PATH . '/files/chronological_contributor_list.php');
 include_once(INCLUDES_PATH . '/files/read_contributor_info.php');
--- includes/messages/welcome-nl-.php
+++ includes/messages/welcome-nl-.php
@@ -1,4 +1,18 @@
 <p>Ik heb gezien dat je je eerste bewerking hebt gemaakt op OpenStreetMap. Proficiat, en bedankt! Bij deze ben je ook lid van de OSM-gemeenschap.</p>
+/* This file is part of osm-welcome: a platform to coordinate welcoming of OpenStreetMap mappers
+ * Copyright © 2018  Midgard and osm-welcome contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 <p>Wij bestaan niet enkel <?php markdown_link('virtueel', 'http://osm.be/'); ?>, maar ook <?php markdown_link('in het echt', 'https://www.meetup.com/OpenStreetMap-Belgium/'); ?>.</p>
 
