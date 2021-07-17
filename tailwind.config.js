47b3c5b8e798c8790928fe75b1d7c53aa6145fe0
Jonathan Beliën <dev@jbelien.be>
2021-07-17 12:57:19 +0200

    Front-end configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './templates/**/*.html.twig',
+    './assets/**/*.ts',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.ts')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -57,6 +57,8 @@ Encore
 
     // enables Sass/SCSS support
     // .enableSassLoader()
+    // enables PostCSS support
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
