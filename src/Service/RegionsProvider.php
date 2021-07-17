6b53c58b05edef070024e49ceed23e84acd5dcb8
Jonathan Beliën <dev@jbelien.be>
2021-07-17 17:04:21 +0200

    Create RegionsProvider.php

---
+++ src/Service/RegionsProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Yaml\Yaml;
+
+class RegionsProvider
+{
+    private array $regions = [];
+
+    public function __construct()
+    {
+        $yaml = Yaml::parseFile('config/regions.yaml');
+
+        $this->regions = $yaml['regions'] ?? [];
+    }
+
+    public function getRegions(): array
+    {
+        return $this->regions;
+    }
+
+    public function getRegion(string $key): array|null
+    {
+        return $this->regions[$key] ?? null;
+    }
+
+    public function getGeometry(string $key): string|null
+    {
+        $path = sprintf('assets/%s.geojson', $key);
+        if (!file_exists($path) || !is_readable($path)) {
+            return null;
+        }
+
+        return file_get_contents($path);
+    }
+}
