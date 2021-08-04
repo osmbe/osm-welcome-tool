1194b2a0e8bb5978172e65ad3cdbb01393729b8e
Jonathan Beliën <dev@jbelien.be>
2021-08-04 17:48:08 +0200

    🏗️ Update RegionsProvider

--- src/Service/RegionsProvider.php
+++ src/Service/RegionsProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use Exception;
 use Symfony\Component\Yaml\Yaml;
 
 class RegionsProvider
@@ -20,16 +21,23 @@ class RegionsProvider
         return $this->regions;
     }
 
-    public function getRegion(string $key): array|null
+    public function getRegion(string $key): array
     {
-        return $this->regions[$key] ?? null;
+        if (!isset($this->regions[$key])) {
+            throw new Exception(sprintf('Key "%s" is not defined in regions configuration file.', $key));
+        }
+
+        $region = $this->regions[$key];
+        $region['key'] = $key;
+
+        return $region;
     }
 
-    public function getGeometry(string $key): string|null
+    public function getGeometry(string $key): string
     {
         $path = sprintf('%s/assets/regions/%s.geojson', $this->projectDirectory, $key);
         if (!file_exists($path) || !is_readable($path)) {
-            return null;
+            throw new Exception(sprintf('Geometry is not defined for region "%s".', $key));
         }
 
         return file_get_contents($path);
