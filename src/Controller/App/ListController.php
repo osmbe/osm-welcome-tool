9c86fba575d8f0d754653b2300959902a81265f9
Jonathan Beliën <dev@jbelien.be>
2021-08-04 21:52:22 +0200

    ✨ Update ListController
    Add filter per year & month.

--- src/Controller/App/ListController.php
+++ src/Controller/App/ListController.php
@@ -4,7 +4,8 @@ namespace App\Controller\App;
 
 use App\Entity\Mapper;
 use App\Service\RegionsProvider;
-use Exception;
+use DateTime;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,15 @@ class ListController extends AbstractController
         private RegionsProvider $provider,
     ) {}
 
-    #[Route('/{regionKey}/list', name: 'app_list')]
-    public function index(string $regionKey): Response
+    #[Route('/{regionKey}/list/{year}/{month}', name: 'app_list')]
+    public function index(string $regionKey, ?int $year = null, ?int $month = null): Response
     {
         $region = $this->provider->getRegion($regionKey);
+
+        if (is_null($year) && is_null($month)) {
+            $year = intval(date('Y'));
+            $month = intval(date('m'));
+        }
 
         /** @var Mapper[] */
         $mappers = $this->getDoctrine()
@@ -28,9 +34,21 @@ class ListController extends AbstractController
         $firstChangetsetCreatedAt = array_map(function (Mapper $mapper) { return $mapper->getChangesets()->first()->getCreatedAt(); }, $mappers);
         array_multisort($firstChangetsetCreatedAt, SORT_DESC, $mappers);
 
+        $month = (new DateTime())->setDate($year, $month, 1);
+
+        $mappers = array_filter(
+            $mappers,
+            function (Mapper $mapper) use ($month) {
+                /** @var DateTimeImmutable */
+                $createdAt = $mapper->getChangesets()->first()->getCreatedAt();
+                return $createdAt->format('Ym') === $month->format('Ym');
+            }
+        );
+
         return $this->render('app/list/index.html.twig', [
-            'region' => $region,
+            'region'  => $region,
             'mappers' => $mappers,
+            'month'   => $month
         ]);
     }
 }
