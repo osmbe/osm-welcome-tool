121c2e18ac024c1aa30bd3ccdd69905a72843211
Jonathan Beliën <dev@jbelien.be>
2021-08-05 08:58:15 +0200

    ✨ Create MapperProvider
    Convert OSM response to Mapper entity.

---
+++ src/Service/MapperProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Mapper;
+use App\Repository\MapperRepository;
+use DateTime;
+
+class MapperProvider
+{
+    public function __construct(
+        private MapperRepository $repository
+    ) {
+    }
+
+    public function fromOSM(array $array): Mapper
+    {
+        $mapper = $this->repository->find($array['user']['id']);
+        if ($mapper === null) {
+            $mapper = new Mapper();
+            $mapper->setId($array['user']['id']);
+            $mapper->setAccountCreated(new DateTime($array['user']['account_created']));
+            $mapper->setStatus('new');
+            // $mapper->setRegion($region);
+        }
+
+        $mapper->setChangesetsCount($array['user']['changesets']['count']);
+        $mapper->setDisplayName($array['user']['display_name']);
+        $mapper->setImage($array['user']['img']['href'] ?? null);
+
+        return $mapper;
+    }
+}
