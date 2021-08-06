a959736120ebaf90692fad1ff252f0b27c498acb
Jonathan Beliën <dev@jbelien.be>
2021-08-06 14:19:50 +0200

    🍱 Move app.ts to typescript folder

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.ts')
+    .addEntry('app', './assets/typescript/app.ts')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
