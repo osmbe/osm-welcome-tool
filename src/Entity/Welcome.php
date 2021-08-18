ee8b17d9cbcf717cb2706c05f1f817fdfc80d84b
Jonathan Beliën <dev@jbelien.be>
2021-08-18 15:25:37 +0200

    🗃 Update Welcome entity

--- src/Entity/Welcome.php
+++ src/Entity/Welcome.php
@@ -33,6 +33,11 @@ class Welcome
      */
     private $reply;
 
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, cascade={"persist"})
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -70,6 +75,18 @@ class Welcome
     public function setReply(?\DateTimeInterface $reply): self
     {
         $this->reply = $reply;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
