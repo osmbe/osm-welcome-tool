a0187eae4a3fd67c72d0de932c72b1b964bfc0c9
Jonathan Beliën <dev@jbelien.be>
2021-08-19 17:48:38 +0200

    ⬆️ Add React & HeadlessUI

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ Encore
     .enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
