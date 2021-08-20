fe3c81b15a5a22f67dfa304577b6d656c1a8ba89
jbelien <jbelien@users.noreply.github.com>
2021-08-20 14:05:40 +0000

    Apply fixes from StyleCI

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context): Kernel {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
