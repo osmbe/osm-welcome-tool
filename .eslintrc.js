dae5bcef5f0980b347e4b0ae266978992bde5056
Jonathan Beliën <dev@jbelien.be>
2021-08-28 09:57:25 +0200

    🔧 Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,29 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': ['error'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   }
 };
