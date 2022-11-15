d2cb85205688e760150912871490129b0493e53e
Jonathan Beliën <dev@jbelien.be>
2022-11-15 17:13:43 +0100

    Update NewMapperCommand.php

--- src/Command/NewMapperCommand.php
+++ src/Command/NewMapperCommand.php
@@ -85,10 +85,7 @@ class NewMapperCommand extends Command
             $changesetsCollection = $changesetsResponse->toArray();
 
             $path = 'var/users_deleted.txt';
-            if (!file_exists($path)) {
-                $filesystem = new Filesystem();
-                $filesystem->dumpFile($path, $this->osm->getDeletedUsers()->getContent());
-            }
+            (new Filesystem())->dumpFile($path, $this->osm->getDeletedUsers()->getContent());
             $usersDeleted = array_map(fn ($line) => (int) trim($line), file($path, \FILE_IGNORE_NEW_LINES | \FILE_SKIP_EMPTY_LINES));
 
             $features = array_filter($changesetsCollection['features'], fn (array $feature) => !\in_array((int) $feature['properties']['uid'], $usersDeleted, true));
