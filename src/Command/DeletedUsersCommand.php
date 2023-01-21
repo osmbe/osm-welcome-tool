402a4d999dd9f88f3a54efc98026ff1657d913a9
Jonathan Beliën <dev@jbelien.be>
2023-01-21 11:25:06 +0100

    Update DeletedUsersCommand.php

--- src/Command/DeletedUsersCommand.php
+++ src/Command/DeletedUsersCommand.php
@@ -2,41 +2,38 @@
 
 namespace App\Command;
 
-use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(
     name: 'osm:deleted-users',
-    description: 'Download and cache OpenStreetMap deleted users',
+    description: 'Download OpenStreetMap deleted users',
 )]
 class DeletedUsersCommand extends Command
 {
-    final public const CACHE_KEY = 'users_deleted';
+    // final public const CACHE_KEY = 'users_deleted';
 
     public function __construct(
-        private readonly HttpClientInterface $client,
+        // private readonly HttpClientInterface $client,
         private readonly Filesystem $filesystem,
-        private readonly CacheItemPoolInterface $cache
+        // private readonly CacheItemPoolInterface $cache
     ) {
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this->addOption(
-            'force',
-            'f',
-            InputOption::VALUE_NONE,
-            'Force process (even if it has already been cached)'
-        );
-    }
+    // protected function configure(): void
+    // {
+    //     $this->addOption(
+    //         'force',
+    //         'f',
+    //         InputOption::VALUE_NONE,
+    //         'Force process (even if it has already been cached)'
+    //     );
+    // }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
