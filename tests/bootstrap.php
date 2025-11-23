7befddad16ffbf7202aab14c02d89f0eebac2452
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-11-23 16:29:46 +0100

    Apply PHPStan fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+if (method_exists(Dotenv::class, 'bootEnv')) { // @phpstan-ignore function.alreadyNarrowedType
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if (isset($_SERVER['APP_DEBUG'])) {
     umask(0000);
 }
