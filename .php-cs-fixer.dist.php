1f1f49c0414724a12f3dd2461e32003904467538
Jonathan Beliën <dev@jbelien.be>
2026-05-16 11:41:10 +0200

    🔨 Update friendsofphp/php-cs-fixer recipe

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,17 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('node_modules')
-    ->exclude('var');
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         '@Symfony' => true,
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+;
